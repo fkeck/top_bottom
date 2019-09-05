05caf82c0a58e122f61a0fef3709710f1613885b
fkeck <francois.keck@gmail.com>
2019-09-05 19:01:26 +0200

    Compute richness estimators

--- R/AN_Richness.R
+++ R/AN_Richness.R
@@ -28,3 +28,28 @@ rar_richness <- com_merged_raw_list %>%
 
 
 
+
+es_richness <- com_merged_raw_list %>% 
+  mutate(ES_RICHNESS = map(data, function(data){
+    spread_cdm(data, LAC_POS, TAXON, COUNT) %>%
+      estimateR() %>%
+      t() %>%
+      set_colnames(c("S_OBS", "S_CHAO1", "SE_CHAO1", "S_ACE", "SE_ACE")) %>% 
+      as_tibble(rownames = "LAC_POS") %>% 
+      left_join(lakes_meta_nr)
+  })) %>% 
+  mutate(MEAN_S_CHAO1 = map(ES_RICHNESS, function(ES_RICHNESS){
+    ES_RICHNESS %>% 
+      group_by(POSITION) %>% 
+      summarise(MEAN_RAR_RICH = mean(S_CHAO1))
+  })) %>% 
+  mutate(PLOT_S_CHAO1 = map(ES_RICHNESS, function(ES_RICHNESS){
+    ES_RICHNESS %>% 
+      ggplot() +
+      geom_boxplot(aes(POSITION, S_CHAO1))
+  }))%>% 
+  mutate(TEST_S_CHAO1 = map(ES_RICHNESS, function(ES_RICHNESS){
+    val_top <- ES_RICHNESS$S_CHAO1[ES_RICHNESS$POSITION == "Top"]
+    val_bot <- ES_RICHNESS$S_CHAO1[ES_RICHNESS$POSITION == "Bottom"]
+    wilcox.test(val_top, val_bot, paired = TRUE)
+  }))
