98fdaa38c79c0635a821102d5a8197f135d040b6
fkeck <francois.keck@gmail.com>
2020-04-05 14:41:40 +0200

    Fix error probably caused by an update of vegan

--- R/AN_PERMANOVA_paired.R
+++ R/AN_PERMANOVA_paired.R
@@ -8,17 +8,17 @@ adonis_blocks <- function(x, condition, permutation = 999, method = "bray", bloc
   formula <- as.formula("x ~ condition")
   fit <- adonis(formula = formula, permutations = 1, method = method, ...)
   pop <- rep(NA, permutation + 1)
-  pop[1] <- fit$aov.tab[1, 5]
+  pop[1] <- fit$R2[1]
   ctrl <- how(blocks = blocks, within = Within(type = "series", mirror = FALSE))
   nobs <- nrow(x)
   formula_rand <- update(formula, . ~ .[idx])
   for(i in 2:(permutation + 1)) {
     idx <- shuffle(nobs, control = ctrl)
     fit_rand <- adonis(formula = formula_rand, permutations = 1, method = method, ...)
-    pop[i] <- fit_rand$aov.tab[1, 5]
+    pop[i] <- fit_rand$R2[1]
   }
   pval <- sum(pop >= pop[1]) / (permutation + 1)
-  fit$aov.tab["condition", "Pr(>F)"] <- pval
+  fit$`Pr(>F)`[1] <- pval
   return(fit)
 }
 
