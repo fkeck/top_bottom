7cf038710a3feda6639b6ff66fe11dd798b366aa
fkeck <francois.keck@gmail.com>
2020-04-20 15:04:36 +0200

    Fix missing data following new corrected file for CN

--- R/AN_Sediment_basics_TB.R
+++ R/AN_Sediment_basics_TB.R
@@ -15,8 +15,7 @@ dat <- nmds_rar_list %>%
   left_join(lakes_cn, by = c("LAC", "POSITION")) %>%
   semi_join(lakes_meta_nr, by = c("LAC", "POSITION")) %>% 
   left_join(lakes_dna) %>% 
-    filter(POSITION != "Bottom 2",
-           !LAC %in% c("Chalain")) %>% 
+    filter(POSITION != "Bottom 2") %>% 
   mutate(mug_ADN_BY_g_C_DRY_SED = (ng_ADN_BY_g_DRY_SED/1000) * (1/(C_tot_perc_MS/100)),
          mug_ADN_BY_g_Corg_DRY_SED = (ng_ADN_BY_g_DRY_SED/1000) * (1/(C_org_perc_MS/100))) %>%
   mutate(jitter_val = runif(nrow(.), -0.1, 0.1))
@@ -28,24 +27,24 @@ sed_basics_plots$DNA <- ggplot(dat) +
   xlab("") + ylab(expression(paste("DNA (", µg~g^-1, " dry sed.)"))) +
   theme_boxplot_sed
 
-sed_basics_plots$DNA_corrected_Ctot <- ggplot(dat) +
-  geom_boxplot(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_C_DRY_SED , group = POSITION, fill = POSITION), outlier.size = 0.5)+
-  geom_line(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_C_DRY_SED, group = LAC), alpha = 0.2) +
-  scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
-  xlab("") + ylab(expression(paste("DNA (", µg~g^-1, "TC dry sed.)"))) +
-  theme_boxplot_sed
-
-sed_basics_plots$DNA_corrected_Corg <- ggplot(dat) +
-  geom_boxplot(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_Corg_DRY_SED , group = POSITION, fill = POSITION), outlier.size = 0.5)+
-  geom_line(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_Corg_DRY_SED, group = LAC), alpha = 0.2) +
-  scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
-  xlab("") + ylab(expression(paste("DNA (", µg~g^-1, "TOC dry sed.)"))) +
-  theme_boxplot_sed
+# sed_basics_plots$DNA_corrected_Ctot <- ggplot(dat) +
+#   geom_boxplot(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_C_DRY_SED , group = POSITION, fill = POSITION), outlier.size = 0.5)+
+#   geom_line(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_C_DRY_SED, group = LAC), alpha = 0.2) +
+#   scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
+#   xlab("") + ylab(expression(paste("DNA (", µg~g^-1, "TC dry sed.)"))) +
+#   theme_boxplot_sed
+# 
+# sed_basics_plots$DNA_corrected_Corg <- ggplot(dat) +
+#   geom_boxplot(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_Corg_DRY_SED , group = POSITION, fill = POSITION), outlier.size = 0.5)+
+#   geom_line(aes(as.numeric(as.factor(POSITION)), mug_ADN_BY_g_Corg_DRY_SED, group = LAC), alpha = 0.2) +
+#   scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
+#   xlab("") + ylab(expression(paste("DNA (", µg~g^-1, "TOC dry sed.)"))) +
+#   theme_boxplot_sed
 
 
 sed_basics_tests$DNA <- wilcox.test((ng_ADN_BY_g_DRY_SED/1000) ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
-sed_basics_tests$DNA_corrected_Ctot <- wilcox.test(mug_ADN_BY_g_C_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
-sed_basics_tests$DNA_corrected_Corg <- wilcox.test(mug_ADN_BY_g_Corg_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
+# sed_basics_tests$DNA_corrected_Ctot <- wilcox.test(mug_ADN_BY_g_C_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
+# sed_basics_tests$DNA_corrected_Corg <- wilcox.test(mug_ADN_BY_g_Corg_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
 
 
 
@@ -55,16 +54,16 @@ dat <- nmds_rar_list %>%
   separate(LAC_POS, into = c("LAC", "POSITION"), sep = "_") %>%
   left_join(lakes_cn, by = c("LAC", "POSITION")) %>% 
   filter(POSITION != "Bottom 2",
-         !LAC %in% c("Bourget", "Léman", "Marion", "Vert")) %>% 
+         !LAC %in% c("Léman", "Vert")) %>% 
   semi_join(lakes_meta_nr, by = c("LAC", "POSITION")) %>% 
   mutate(jitter_val = runif(nrow(.), -0.1, 0.1))
 
-sed_basics_plots$COT <- ggplot(dat) +
-  geom_boxplot(aes(as.numeric(as.factor(POSITION)), C_tot_perc_MS, group = POSITION, fill = POSITION), outlier.size = 0.5)+
-  geom_line(aes(as.numeric(as.factor(POSITION)), C_tot_perc_MS, group = LAC), alpha = 0.2) +
-  scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
-  xlab("") + ylab("Total carbon (% of dry mass)") +
-  theme_boxplot_sed
+# sed_basics_plots$COT <- ggplot(dat) +
+#   geom_boxplot(aes(as.numeric(as.factor(POSITION)), C_tot_perc_MS, group = POSITION, fill = POSITION), outlier.size = 0.5)+
+#   geom_line(aes(as.numeric(as.factor(POSITION)), C_tot_perc_MS, group = LAC), alpha = 0.2) +
+#   scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
+#   xlab("") + ylab("Total carbon (% of dry mass)") +
+#   theme_boxplot_sed
 
 sed_basics_plots$Corg <- ggplot(dat) +
   geom_boxplot(aes(as.numeric(as.factor(POSITION)), C_org_perc_MS, group = POSITION, fill = POSITION), outlier.size = 0.5)+
@@ -73,7 +72,7 @@ sed_basics_plots$Corg <- ggplot(dat) +
   xlab("") + ylab("TOC (% of dry mass)") +
   theme_boxplot_sed
 
-sed_basics_tests$COT <- wilcox.test(C_tot_perc_MS ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
+# sed_basics_tests$COT <- wilcox.test(C_tot_perc_MS ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
 sed_basics_tests$Corg <- wilcox.test(C_org_perc_MS ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
 
 
@@ -83,7 +82,7 @@ dat <- nmds_rar_list %>%
   separate(LAC_POS, into = c("LAC", "POSITION"), sep = "_") %>%
   left_join(lakes_cn, by = c("LAC", "POSITION")) %>% 
   filter(POSITION != "Bottom 2",
-         !LAC %in% c("Bourget", "Léman", "Marion", "Mont Coua", "Vert", "Gentau")) %>% 
+         !LAC %in% c("Bourget", "Léman", "Mont Coua", "Gentau")) %>% 
   semi_join(lakes_meta_nr, by = c("LAC", "POSITION")) %>% 
   mutate(CAROTENOIDS_BY_g_C_DRY_SED = CAROTENOIDS * (1/(C_tot_perc_MS/100)),
          CAROTENOIDS_BY_g_Corg_DRY_SED = CAROTENOIDS * (1/(C_org_perc_MS/100))) %>%
@@ -96,23 +95,23 @@ sed_basics_plots$PIG <- ggplot(dat) +
   xlab("") + ylab(expression(paste("Total carotenoids (", µg~g^-1, " dry sed.)"))) +
   theme_boxplot_sed
 
-sed_basics_plots$PIG_corrected_Ctot <- ggplot(dat) +
-  geom_boxplot(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_C_DRY_SED, group = POSITION, fill = POSITION), outlier.size = 0.5)+
-  geom_line(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_C_DRY_SED, group = LAC), alpha = 0.2) +
-  scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
-  xlab("") + ylab(expression(paste("Total carotenoids (", µg~g^-1, "TC dry sed.)"))) +
-  theme_boxplot_sed
-
-sed_basics_plots$PIG_corrected_Corg <- ggplot(dat) +
-  geom_boxplot(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_Corg_DRY_SED, group = POSITION, fill = POSITION), outlier.size = 0.5)+
-  geom_line(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_Corg_DRY_SED, group = LAC), alpha = 0.2) +
-  scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
-  xlab("") + ylab(expression(paste("Total carotenoids (", µg~g^-1, "TOC dry sed.)"))) +
-  theme_boxplot_sed
+# sed_basics_plots$PIG_corrected_Ctot <- ggplot(dat) +
+#   geom_boxplot(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_C_DRY_SED, group = POSITION, fill = POSITION), outlier.size = 0.5)+
+#   geom_line(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_C_DRY_SED, group = LAC), alpha = 0.2) +
+#   scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
+#   xlab("") + ylab(expression(paste("Total carotenoids (", µg~g^-1, "TC dry sed.)"))) +
+#   theme_boxplot_sed
+# 
+# sed_basics_plots$PIG_corrected_Corg <- ggplot(dat) +
+#   geom_boxplot(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_Corg_DRY_SED, group = POSITION, fill = POSITION), outlier.size = 0.5)+
+#   geom_line(aes(as.numeric(as.factor(POSITION)), CAROTENOIDS_BY_g_Corg_DRY_SED, group = LAC), alpha = 0.2) +
+#   scale_x_continuous(breaks = c(1, 2), labels = c("Past (bottom)", "Recent (top)")) +
+#   xlab("") + ylab(expression(paste("Total carotenoids (", µg~g^-1, "TOC dry sed.)"))) +
+#   theme_boxplot_sed
 
 sed_basics_tests$PIG <- wilcox.test(CAROTENOIDS ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
-sed_basics_tests$PIG_corrected_Ctot <- wilcox.test(CAROTENOIDS_BY_g_C_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
-sed_basics_tests$PIG_corrected_Corg <- wilcox.test(CAROTENOIDS_BY_g_Corg_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
+# sed_basics_tests$PIG_corrected_Ctot <- wilcox.test(CAROTENOIDS_BY_g_C_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
+# sed_basics_tests$PIG_corrected_Corg <- wilcox.test(CAROTENOIDS_BY_g_Corg_DRY_SED ~ POSITION, paired = TRUE, data = dat, exact = FALSE)
 
 
 
